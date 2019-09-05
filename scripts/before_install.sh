db504cbdabb8c73a190b21291651895db0641a79
akusany2 <ankitkumar0411@gmail.com>
2019-09-05 20:04:05 +0530

    add docker install

---
+++ scripts/before_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+docker kill marketingFront
