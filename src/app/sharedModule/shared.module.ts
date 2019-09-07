ae4cd90765999b73f8a5ea65548e9664e0fad56a
akusany2 <ankitkumar0411@gmail.com>
2019-09-07 14:35:56 +0530

    Remove material

--- src/app/sharedModule/shared.module.ts
+++ src/app/sharedModule/shared.module.ts
@@ -1,32 +1,9 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
-import {
-  MatButtonModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatOptionModule,
-  MatSelectModule
-} from "@angular/material";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatOptionModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule
-  ],
-  exports: [
-    MatFormFieldModule,
-    MatSelectModule,
-    MatOptionModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule
-  ],
+  imports: [CommonModule],
+  exports: [],
   declarations: []
 })
 export class SharedModule {}
