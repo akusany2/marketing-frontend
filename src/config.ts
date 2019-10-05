4f12e1ac74835ffa6dea42663aa42ae1d6935eb6
akusany2 <ankitkumar0411@gmail.com>
2019-10-05 15:01:13 +0530

    Add table to audience

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // export const apiServerUrl = "http://192.168.43.129:3000/api";
 // export const apiServerUrl = "http://10.0.226.208:3000/api";
-// export const apiServerUrl = "http://172.31.178.209:3000/api";
-export const apiServerUrl =
-  "http://ec2-34-229-231-51.compute-1.amazonaws.com:3000/api";
+export const apiServerUrl = "http://172.31.178.209:3000/api";
+// export const apiServerUrl =
+  // "http://ec2-34-229-231-51.compute-1.amazonaws.com:3000/api";
