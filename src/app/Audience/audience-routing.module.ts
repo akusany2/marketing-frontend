9e2f9a56ce2ed6abe15aa74add9601fb09764fd2
akusany2 <ankitkumar0411@gmail.com>
2019-12-24 21:10:28 +0530

    Authguard to all routes

--- src/app/Audience/audience-routing.module.ts
+++ src/app/Audience/audience-routing.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { NotFoundComponent } from "../SharedComponent/not-found.component";
 import { AuthGuard } from "../sharedServices/auth.guard";
 import { AudienceComponent } from "./audience.component";
 import { AudienceDetailComponent } from "./AudienceDetails/audience-details.component";
 
 const routes: Routes = [
   {
-    path: "",
-    component: AudienceComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: "create",
-    component: AudienceDetailComponent,
-    canActivate: [AuthGuard]
-  },
-  { path: "**", component: NotFoundComponent }
+    path: "", canActivate: [AuthGuard],
+    children: [
+      {
+        path: "",
+        component: AudienceComponent
+      },
+      {
+        path: "create",
+        component: AudienceDetailComponent
+      },
+      {
+        path: "edit",
+        component: AudienceDetailComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
