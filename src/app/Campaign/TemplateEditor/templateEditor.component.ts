9c874545a0c39915fb942f0829ffe3824f3804f0
ankumar <ankumar@IND116>
2020-02-06 18:59:48 +0530

    Template editor init

--- src/app/Campaign/TemplateEditor/templateEditor.component.ts
+++ src/app/Campaign/TemplateEditor/templateEditor.component.ts
@@ -1,20 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { CampaignQuery } from '../campaign.store';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { CampaignQuery } from "../campaign.store";
 
 @Component({
-  selector: 'app-template-editor',
-  templateUrl: './templateEditor.component.html',
-  styleUrls: ['./templateEditor.component.css']
+  selector: "app-template-editor",
+  templateUrl: "./templateEditor.component.html",
+  styleUrls: ["./templateEditor.component.css"]
 })
 export class TemplateEditorComponent implements OnInit {
-
-  constructor(private campaignQuery: CampaignQuery, private router: Router) { }
+  templateEditForm: FormGroup;
+  iframeDoc;
+  // primaryText: string;
+  // secondaryText: string;
+  constructor(private campaignQuery: CampaignQuery, private router: Router) {}
 
   ngOnInit() {
+    this.templateEditForm = new FormGroup({
+      templateName: new FormControl("", [Validators.required]),
+      primaryText: new FormControl("Primary text", [Validators.required]),
+      secondaryText: new FormControl("Secondary text", [Validators.required])
+    });
+
     if (!this.campaignQuery.getValue().templateHtml) {
-      this.router.navigate(['/campaign']);
+      this.router.navigate(["/campaign"]);
+    } else {
+      this.readyTemplatePreview();
+      this.handleTextChanges();
     }
   }
 
+  readyTemplatePreview() {
+    this.iframeDoc = document.getElementById(
+      "templatePreview"
+    ).contentWindow.document;
+    this.iframeDoc.open();
+    this.iframeDoc.write(this.campaignQuery.getValue().templateHtml);
+    this.iframeDoc.close();
+  }
+
+  handleTextChanges() {
+    let primaryText = this.iframeDoc.querySelector("#primaryText");
+    let secondaryText = this.iframeDoc.querySelector("#secondaryText");
+    this.templateEditForm.valueChanges.subscribe(form => {
+      primaryText.innerHTML = form.primaryText;
+      secondaryText.innerHTML = form.secondaryText;
+    });
+  }
 }
