22206c3b6724b09f8399dae34fae933104a97699
ankumar <ankumar@IND116>
2020-03-18 12:43:41 +0530

    Update dist

--- src/app/Dashboard/dashboard.component.ts
+++ src/app/Dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { UserProfileInterface } from "../Login/interfaces/user.interface";
 import { UserService } from "../sharedServices/user.service";
 
 @Component({
@@ -7,10 +9,10 @@ import { UserService } from "../sharedServices/user.service";
   styleUrls: ["./dashboard.component.css"]
 })
 export class DashboardComponent implements OnInit {
-  userProfile$;
+  userProfile$: Observable<UserProfileInterface>;
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userProfile$ = this.userService.getUserProfile();
+    this.userProfile$ = this.userService.getUserProfile() as any;
   }
 }
